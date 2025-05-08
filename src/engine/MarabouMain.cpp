d690b6f2a36e54aee520f237e06d769e4dd04a13
mayaswissa <mayaswissa@gmail.com>
2025-05-08 11:13:53 +0300

    const bool GlobalConfiguration::PREPROCESSOR_ELIMINATE_VARIABLES = true;

--- src/engine/MarabouMain.cpp
+++ src/engine/MarabouMain.cpp
@@ -236,7 +236,7 @@ int marabouMain( int argc, char **argv )
             std::string exampleID;
             extractExampleID( examplePath, exampleID );
             auto txtOutputFilePath = Options::get()->getString( Options::DQN_OUTPUT_FILE_PATH );
-            currentRunFile << std::string( txtOutputFilePath.ascii() ) << exampleID << ".txt";
+            currentRunFile << std::string( txtOutputFilePath.ascii() ) << ".txt";
             std::ofstream outFile( currentRunFile.str(), std::ios::out | std::ios::app );
 
             if ( outFile.is_open() )
