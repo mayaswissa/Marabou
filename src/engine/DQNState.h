0f1b3a6c84f67c9714457aab2477e15393b8d2e3
mayaswissa <mayaswissa@gmail.com>
2025-06-17 18:35:43 +0300

    with soi state

--- src/engine/DQNState.h
+++ src/engine/DQNState.h
@@ -14,10 +14,10 @@ enum DQNFeatures : unsigned {
     DQN_RELU_NOT_FIXED_VALUE = 0,
     DQN_RELU_ACTIVE_VALUE = 1,
     DQN_RELU_INACTIVE_VALUE = 2,
-    // SOI_ACTIVE_SCORE = 3,
-    // SOI_INACTIVE_SCORE = 4,
-    POLARITY_SCORE = 3,
-    BaBsr_SCORE = 4,
+    SOI_ACTIVE_SCORE = 3,
+    SOI_INACTIVE_SCORE = 4,
+    POLARITY_SCORE = 5,
+    BaBsr_SCORE = 6,
 
     NUM_FEATURES
 };
@@ -31,7 +31,7 @@ public:
 
     torch::Tensor toTensor() const;
     void updateConstraintPhase( unsigned constraintIndex, unsigned newPhase );
-    // void updateSoIScoreForAgent( unsigned constraintIndex, double SoiActiveScore, double SoiInactiveScore );
+    void updateSoIScoreForAgent( unsigned constraintIndex, double SoiActiveScore, double SoiInactiveScore );
     void updateBounds( unsigned constraintIndex, double upperBound, double lowerBound );
     void updatePolarity( unsigned constraintIndex, double polarityScore );
     void updateBaBsrScore( unsigned constraintIndex, double BaBsrScore );
