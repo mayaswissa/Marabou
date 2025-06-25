6ae190867ea8afe2082d22b007b45cd9fb50836a
mayaswissa <mayaswissa@gmail.com>
2025-06-25 17:32:19 +0300

    initial splitting strategy set to pseudo-impact

--- src/configuration/Options.cpp
+++ src/configuration/Options.cpp
@@ -90,7 +90,7 @@ void Options::initializeDefaultValues()
     _stringOptions[PROPERTY_FILE_PATH] = "";
     _stringOptions[INPUT_QUERY_FILE_PATH] = "";
     _stringOptions[SUMMARY_FILE] = "";
-    _stringOptions[SPLITTING_STRATEGY] = "DQN-agent";
+    _stringOptions[SPLITTING_STRATEGY] = "pseudo-impact";
     _stringOptions[SNC_SPLITTING_STRATEGY] = "auto";
     _stringOptions[SYMBOLIC_BOUND_TIGHTENING_TYPE] = "deeppoly";
     _stringOptions[MILP_SOLVER_BOUND_TIGHTENING_TYPE] = "none";
