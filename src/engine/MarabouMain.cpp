57e757ee76cedfadd2b36f11b687706c981de3c3
mayaswissa <mayaswissa@gmail.com>
2025-07-20 18:46:18 +0300

    minor

--- src/engine/MarabouMain.cpp
+++ src/engine/MarabouMain.cpp
@@ -451,7 +451,8 @@ int marabouMain( int argc, char **argv )
             const std::string outDir = options->getString( Options::DQN_OUTPUT_FILE_PATH ).ascii();
 
             // Open summary file
-            const std::string summaryFile = outDir + "/" + exampleID + ".txt";
+            const std::string summaryFile = outDir + "results.txt";
+            options->setString( Options::SUMMARY_FILE, summaryFile );
             std::ofstream out( summaryFile, std::ios::app );
             if ( !out )
             {
