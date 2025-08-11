08e027e116f62f6d243a874f0aca8be995f5dc6b
mayaswissa <mayaswissa@gmail.com>
2025-08-11 15:00:55 +0300

    update parsing for vnnlib properties

--- src/engine/MarabouMain.cpp
+++ src/engine/MarabouMain.cpp
@@ -256,8 +256,8 @@ std::vector<std::pair<std::string, std::string>> collectExamples( std::string &r
         else if ( name.size() >= 4 && name.compare( name.size() - 4, 4, ".txt" ) == 0 )
             fullPath = root + "/" + name;
 
-        else
-            continue;
+        else if ( name.size() >= 4 && name.compare( name.size() - 7, 7, ".vnnlib" ) == 0 )
+            fullPath = root + "/" + name;
         std::string id;
         extractID( fullPath, exampleType, id );
 
