c94e601c6d72ab714150caf020fea9cde9ce4295
mayaswissa <mayaswissa@gmail.com>
2025-08-15 17:16:36 +0300

    push action to replay buffer after its subtree

--- src/engine/DQNReplayBuffer.h
+++ src/engine/DQNReplayBuffer.h
@@ -163,9 +163,9 @@ public:
     torch::Tensor getRewards();
     torch::Tensor getNextStates();
     torch::Tensor getDones();
-    long getDemoFactor() const;
-    long getEpsilonDemo() const;
-    long getEpsilonAgent() const;
+    float getDemoFactor() const;
+    float getEpsilonDemo() const;
+    float getEpsilonAgent() const;
     long getEpsilon() const;
 
 
